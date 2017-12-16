0a9251fd815a8d0765699fa12e942985bd42a455
Nicolai-Messing <34276954+Nicolai-Messing@users.noreply.github.com>
2017-12-16 10:52:37 +0100

    # This is a combination of 6 commits. # This is the 1st commit message:
    Update SplitStepOperator.py
    # This is the commit message #2:
    
    Update SplitStepOperator.py
    # This is the commit message #3:
    
    Create FourierTrafoFreddy
    # This is the commit message #4:
    
    Update SplitStepOperator.py
    # This is the commit message #5:
    
    Update SplitStepOperator.py
    # This is the commit message #6:
    
    Update SplitStepOperator.py

---
+++ HaPPPy/Transmission/Modules/SplitStepOperator.py
@@ -0,0 +1,49 @@
+#Split-Step-Algorithm repeated M times
+import numpy
+import cmath
+
+#diagonalelemts of kinetic operator in k-space
+def TElement(ki):
+    
+    Telement = cmath.exp(-1j*hbar*dt*(ki**2)/(4*me))
+    
+    return Telement
+
+#diagonalelements of potential in position-space
+def VElement(Vi):
+    
+    Velement = cmath.exp(-1j*dt*Vi/hbar)
+    
+    return Velement
+
+#SplitStep-Operator
+def splitStep(wave):
+    
+    #transform into k space
+    kWave1 = FT(wave)
+    
+    #applying modified kinetic operator
+    TkWave1 = numpy.multiply(kWave1,T, dtype=numpy.complex64)
+    
+    #transform into x space
+    xWave = IFT(TkWave1)
+    
+    #applying potential operator
+    VxWave = numpy.multiply(xWave1,V, dtype=numpy.complex64)
+    
+    return VxWave
+
+dt = 1 #timestep
+me = 1 #mass of electron
+hbar = 1 #reduced plack constant
+
+positions = [1]
+waveNumbers = [1]
+
+potential = [1] #potential
+
+#diagonalelements of kinetic operator
+T = [TElement(waveNumber) for waveNumber in waveNumbers]
+
+#diagonalelements of potential operator
+V = [VElement(vi) for vi in potential]
