1ce027113bc644b095d2130bf0abac6b7c21480e
fweissle <34276979+fweissle@users.noreply.github.com>
2017-12-16 20:49:27 +0100

    generalized symmetrie point

--- HaPPPy/Transmission/Modules/GaussianWave.py
+++ HaPPPy/Transmission/Modules/GaussianWave.py
@@ -5,26 +5,28 @@ me   = codata.value("electron mass energy equivalent in MeV") * 1e9 ;  # Convert
 hbar = codata.value("Planck constant over 2 pi in eV s")      * 1e15;  # Convert to ps
 
 class GaussianWave():
-    def CreatePackagePoint(self, x, energy, a):
+    def CreatePackagePoint(self, x, x0, energy, a):
         k = cmath.sqrt(2*me*energy)/hbar
         norm = (2/(cmath.pi*(a**2)))**(1/4)
         
-        psi = complex(norm*cmath.exp(1j*k*x-(x/a)**2))
+        psi = complex(norm*cmath.exp(1j*k*(x-x0)-((x-x0)/a)**2))
         
         return psi
     
-    def createPackage(positions, energy, a):
-        phi = [createPackagePoint(x, energy, a) for x in positions]
+    def createPackage(positions, x0, energy, a):
+        phi = [createPackagePoint(x, x0, energy, a) for x in positions]
 
         return phi
     
  '''
- Creates a gaussian package in position-space symmetrically around the origin at time zero.
+ Creates a gaussian package in position-space symmetrically around x0 at time zero.
  
  Parameters
  ----------
  x : float
     Position on which the gaussian wave is evaluated
+ x0 : float
+    symmetry point of the package
  positions : array
     A sequence of positions for which to create the package
  energy: float
