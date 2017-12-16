69caf4ca66f08721427f79e4339b103adeed0468
Nicolai-Messing <34276954+Nicolai-Messing@users.noreply.github.com>
2017-12-16 14:32:55 +0100

    Some fundamental structurechanges
    Added Class SplitStepMethod, definded the parameters first, renamed functions
    Documentation will be added tomorrow by me (Nico).

--- HaPPPy/Transmission/Modules/SplitStepOperator.py
+++ HaPPPy/Transmission/Modules/SplitStepOperator.py
@@ -2,22 +2,49 @@
 import numpy
 import cmath
 
+
+
+dt = 1 #timestep
+me = 1 #mass of electron
+hbar = 1 #reduced plack constant
+
+positions = [1]
+wave_numbers = [1]
+
+potential = [1] #potential
+
+#diagonalelements of kinetic operator
+T = [t_element(wave_number) for wave_number in wave_numbers]
+
+#diagonalelements of potential operator
+V = [v_element(vi) for vi in potential]
+
+
+
+
+
+
+
+
 #diagonalelemts of kinetic operator in k-space
-def t_element(ki):
+class SplitStepMethod():
+    
+    #diagonalelemts of kinetic operator in k-space
+    def t_element.create(ki):
     
     t_element = cmath.exp(-1j*hbar*dt*(ki**2)/(4*me))
     
     return t_element
 
 #diagonalelements of potential in position-space
-def v_element(Vi):
+    def v_element.create(Vi):
     
     v_element = cmath.exp(-1j*dt*vi/hbar)
     
     return v_element
 
 #SplitStep-Operator
-def splitStep(wave):
+    def split_step_method(wave):
     
     #transform into k space
     k_wave = FT(wave)
@@ -42,17 +69,3 @@ def splitStep(wave):
     
     return x_wave_new
 
-dt = 1 #timestep
-me = 1 #mass of electron
-hbar = 1 #reduced plack constant
-
-positions = [1]
-wave_numbers = [1]
-
-potential = [1] #potential
-
-#diagonalelements of kinetic operator
-T = [t_element(wave_number) for wave_number in wave_numbers]
-
-#diagonalelements of potential operator
-V = [v_element(vi) for vi in potential]
