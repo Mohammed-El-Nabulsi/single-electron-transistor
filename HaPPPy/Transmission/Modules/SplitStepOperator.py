8cbf8d90c7573511364adf31904d8a5a95aabfba
Mohammed <Mohammed.El-Nabulsi@outlook.de>
2017-12-17 22:15:21 +0000

    Added implementation

--- HaPPPy/Transmission/Modules/SplitStepOperator.py
+++ HaPPPy/Transmission/Modules/SplitStepOperator.py
@@ -1,64 +1,41 @@
 import numpy
 import cmath
 from scipy.constants import codata
-
-
+from numpy.fft import fft, ifft
 
 
 dt = 10**-15
 me   = codata.value("electron mass energy equivalent in MeV") * 1e9 ;  # Convert to milli eV
 hbar = codata.value("Planck constant over 2 pi in eV s")      * 1e15;  # Convert to p    
-x =[1, 2, 3, 4, 5, 6] # example, getting it from Mo
-k = FT(x)
-v = [1, 2, 3, 4, 5, 6] #example, gettoing from mo
-    
-    
-    
     
 class SplitStepMethod:
+    def __init__(V, x):
+        def create_v_element(self, v_i):
+            return cmath.exp(-1j*dt*v_i/hbar)
+        
+        def create_t_element(self, k_i):
+            return cmath.exp(-1j*hbar*dt(k_i**2)/(4*me))
 
-    
-    
+        k = fft(x)
+
+        self.v_elements = [create_v_element(v_i) for v_i in V]
+        self.t_elements = [create_t_element(k_i) for k_i in k]
+
+
     def use(self, psi):
+        psi_k = fft(psi, norm='ortho')
         
+        psi_tk = numpy.multiply(psi_k, self.t_elements,  dtype=numpy.complex64)
         
-
-        def v_element_create(self, vi):
-       
+        psi_x = ifft(psi_tk, norm='ortho')
         
-            v_element = cmath.exp(-1j*dt*vi/hbar)
+        psi_vx = numpy.multiply(psi_x, self.v_elements, dtype=numpy.complex64)
         
-            return v_element
+        psi_k_new = fft(psi_vx, norm='ortho')
         
-        v_elements = [v_element_create(vi) for vi in v]
+        psi_tk_new = numpy.multiply(psi_k_new, self.t_elements, dtype=numpy.complex64)
         
-        
-        
-        
-        
-        def t_element_create(self, ki):
-            
-            t_element = cmath.exp(-1j*hbar*dt(ki**2)/(4*me))
-            
-            return t_element
-        
-        t_elements = [t_element_create(ki) for ki in k]
-            
-            
-            
-        psi_k = FT(psi)
-        
-        psi_tk = numpy.multiply(psi_k, t_elements,  dtype=numpy.complex64)
-        
-        psi_x = IFT(psi_tk)
-        
-        psi_vx = numpy.multiply(psi_x, v_elements, dtype=numpy.complex64)
-        
-        psi_k_new = FT(psi_vx)
-        
-        psi_tk_new = numpy.multiply(psi_k_new, t_elements, dtype=numpy.complex64)
-        
-        psi_x_new = IFT(psi_tk_new)
+        psi_x_new = ifft(psi_tk_new, norm='ortho')
         
         return psi_x_new
     
