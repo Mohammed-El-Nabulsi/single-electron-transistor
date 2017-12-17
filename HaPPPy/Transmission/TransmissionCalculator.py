34673d8b991af337fd78b241d8d8feea16076101
Mohammed <Mohammed.El-Nabulsi@outlook.de>
2017-12-17 22:33:47 +0000

    Added initial draft

--- HaPPPy/Transmission/TransmissionCalculator.py
+++ HaPPPy/Transmission/TransmissionCalculator.py
@@ -14,12 +14,33 @@ class TransmissionCalculator:
 
         print("Hello from the TransmissionCalculator, what can I do for you?")
 
-    def doCalculation(self):
+    def doCalculation(self, E, V, x):
         """ Some dummy calculation
         
         Returns:
             double. The result
 
         """
+        
+        a = 1;
+
+        psi_0 = GaussianWave().CreatePackage(a, E, x) 
+
+        delta = infinity;
+        margin = 10e-3
+ 
+        while (delta > margin):
+            if (Init):
+                psi_n = psi0
+
+            psi_n1 = SplitStepOperator().Use(psi_n)
+
+            delta = GetLargestDiff(psi_n, psi_n1)
+
+            psi_n = psi_n1
+
+        return CalculateTransmission().Calculate(psi_n, V)
+
+        
 
         return 1.0+1.0
