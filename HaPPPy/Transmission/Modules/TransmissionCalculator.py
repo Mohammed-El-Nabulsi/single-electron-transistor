fa9abee7954965eb390ed0085ff049a441de7019
Jdiarra <34276981+Jdiarra@users.noreply.github.com>
2017-12-18 17:00:25 +0100

    Update TransmissionCalculator.py

--- HaPPPy/Transmission/Modules/TransmissionCalculator.py
+++ HaPPPy/Transmission/Modules/TransmissionCalculator.py
@@ -1,15 +1,17 @@
 import numpy
-#Integration by addition of sample points.
-def integrate(funktion,start): # funktion = array with the samplepoints from the funktion , start sets the startingpoint for the integration.
-    n = start
-    a = 0
-    # Squares the absolut value and adds the samplepoints.
-    while n < numpy.size(funktion) : 
-        a = a + funktion[n]*funktion.conjugate()[n]
-        n = n+1
-    return a
-    
-    # Calculates the propability for tunneling with a given final wavefunktion.
-def Propability(funktion,start):#  Requires only the final wavefunktion in the form of an array and the startingpoint for the propability.
-    x = integrate(funktion,start)/ integrate(funktion,0)
-    return x
+
+class TransmissionCalculator():
+    def integrate(wavefunktion,start): 
+        """ Documentation"""
+        n = start
+        a = 0
+       while n < numpy.size(funktion) : 
+            a = a + wavefunktion[n]*wavefunktion.conjugate()[n]
+            n = n+1
+        return a
+
+        
+    def calculate(wavefunktion,start):
+        """Documentation"""
+        x = TransmissionCalculator.integrate(wavefunktion,start) / TransmissionCalculator.integrate(wavefunktion,0)
+        return x.real
