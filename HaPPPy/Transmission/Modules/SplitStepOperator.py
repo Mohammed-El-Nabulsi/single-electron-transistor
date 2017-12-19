6ffbb29860a24158293b23c8d0bdf5caf64ec833
fweissle <34276979+fweissle@users.noreply.github.com>
2017-12-19 03:00:44 +0100

    using Fourier Class

--- HaPPPy/Transmission/Modules/SplitStepOperator.py
+++ HaPPPy/Transmission/Modules/SplitStepOperator.py
@@ -1,39 +1,41 @@
 import numpy
 import cmath
 from scipy.constants import codata
-from numpy.fft import fft, ifft
+
+from HaPPPy.Transmission.Modules.GaussianWave import Fourier
+
+Fourier = Fourier()
 
 dt = 10**-15
 me   = codata.value("electron mass energy equivalent in MeV") * 1e9 ;  # Convert to milli eV
 hbar = codata.value("Planck constant over 2 pi in eV s")      * 1e15;  # Convert to p    
     
 class SplitStepOperator:
-    def __init__(self, V, x):
+    def __init__(self, V, psi, x, x0, L):
         def create_v_element(v_i):
             return cmath.exp(-1j*dt*v_i/hbar)
         
         def create_t_element(k_i):
             return cmath.exp(-1j*hbar*dt*(k_i**2)/(4*me))
 
-        k = fft(x)
+        k = Fourier.waveNumbers(psi, x0, L)
 
         self.v_elements = [create_v_element(v_i) for v_i in V]
         self.t_elements = [create_t_element(k_i) for k_i in k]
 
-
-    def use(self, psi):
-        psi_k = fft(psi, norm='ortho')
+    def use(self, psi, x0, L):
+        psi_k = FT(psi, x0, L)
         
         psi_tk = numpy.multiply(psi_k, self.t_elements,  dtype=numpy.complex64)
         
-        psi_x = ifft(psi_tk, norm='ortho')
+        psi_x = IFT(psi_tk, x0, L)
         
         psi_vx = numpy.multiply(psi_x, self.v_elements, dtype=numpy.complex64)
         
-        psi_k_new = fft(psi_vx, norm='ortho')
+        psi_k_new = FT(psi_vx, x0, L)
         
         psi_tk_new = numpy.multiply(psi_k_new, self.t_elements, dtype=numpy.complex64)
         
-        psi_x_new = ifft(psi_tk_new, norm='ortho')
+        psi_x_new = IFT(psi_tk_new, x0, L)
         
         return psi_x_new
