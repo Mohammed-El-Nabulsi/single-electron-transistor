980fb31eb9d325db1624e7e654a24e91935a1c0a
fweissle <34276979+fweissle@users.noreply.github.com>
2018-01-08 00:50:05 +0100

    multiple fixes

--- HaPPPy/Transmission/Modules/Fourier.py
+++ HaPPPy/Transmission/Modules/Fourier.py
@@ -1,32 +1,66 @@
 import numpy as np
 from scipy.fftpack import fft, fftfreq, fftshift
-
 import matplotlib.pyplot as plt
 
 class Fourier:
     
-    def __init__(self, x_grid, dx):
-        self.N = x_grid.size
-        self.x_grid = x_grid
-        self.dx = dx
+    def __init__(self, N, dx):
+        """
+        Parameters
+        ----------
+        N : Integer
+            len(x_grid), number of position sample points
+        dx : float
+            distance between two neighbouring samplepoints in position space grid
+        """
         
+        self.N = N
+        self.dx = dx
         self.k = self.get_wavenumbers()
         
     def get_wavenumbers(self):
         xf = fftfreq(self.N, self.dx)
         return fftshift(xf)
-    
+
     def dft(self, x_func):
         k_func = fft(x_func)
-        return fftshift(k_func)
-    
-    def plot_dft_abs_squared(self, x_func):
-        dft_func = self.dft(x_func)
-        dft_abs_squared = np.multiply(dft_func, dft_func.conj()).real
+        return fftshift(k_func)*1/self.N
         
-        plot_dft = plt.plot(self.k, dft_abs_squared)
-        plt.title ("Propabilitydensity of the transformed wave in wavenumber-space")
-        plt.xlabel("wavenumber grid")
-        plt.ylabel("probabilitydensity")
-        
+    def plot_dft(self, x_func):
+        dft = self.dft(x_func)
+        plt.plot(self.k,np.multiply(dft,dft.conj()).real)
+        plt.grid()
         plt.show()
+
+
+#for Testing: (works!)
+#fourier = Fourier(500,1/500)
+#
+#x = np.linspace(0, 1, 500)
+#y = np.exp(50.0 * 1.j * 2.0*np.pi*x) + 0.6*np.exp(-25.0 * 1.j * 2.0*np.pi*x)
+#
+#fourier.plot_dft(y)
+#
+### number of signal points
+##N = 200
+##L = 1
+### sample spacing
+##dx = L / (N)
+##
+##x = np.linspace(0.0, N*dx, N)
+##
+##print(N*dx)
+##
+##
+##y = np.exp(50.0 * 1.j * 2.0*np.pi*x) + 0.6*np.exp(-25.0 * 1.j * 2.0*np.pi*x)
+##
+##yf = fft(y)
+##xf = fftfreq(N, dx)
+##xf = fftshift(xf)
+##yplot = fftshift(yf)
+##import matplotlib.pyplot as plt
+###plt.plot(x,np.abs(y))
+##plt.show()
+##plt.plot(xf, 1.0/(N) * np.abs(yplot))
+##plt.grid()
+##plt.show()
