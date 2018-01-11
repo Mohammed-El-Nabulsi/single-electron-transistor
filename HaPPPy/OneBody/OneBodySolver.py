4d328d2eb76438456194e765795158d38128a7ff
Ludwig Hendl <ludwig@4xhendl.de>
2018-01-11 17:27:50 +0100

    test

--- HaPPPy/OneBody/OneBodySolver.py
+++ HaPPPy/OneBody/OneBodySolver.py
@@ -14,7 +14,8 @@ class OneBodySolver:
     hbar = constants.hbar
     me = constants.m_e
     e = constants.e
-    
+    a = ""
+	#aplot = ""
     def __init__(self, l, n):
         """ The constructor.
         """
@@ -23,9 +24,10 @@ class OneBodySolver:
         print("Hello from the OneBody Solver, what can I do for you?")
         # creating array in user input length l and user input elements n and not changeable array for plot x-axis
         # creating transposed array a -> at
-        a = np.linspace((-self.l / 2), (self.l / 2), self.n)
-        aplot = np.linspace((-self.l / 2), (self.l / 2), self.n)
-    
+        self.a = np.linspace((-self.l / 2), (self.l / 2), self.n)
+        #aplot = np.linspace((-self.l / 2), (self.l / 2), self.n)
+        print(self.a)
+        print("der geht")
 
     
 
@@ -45,57 +47,58 @@ class OneBodySolver:
         exit()
     
     
-    def calcualteHarmonocPotential(n, intersection, factor):
+    def calcualteHarmonocPotential(self, n, intersection, factor):
+        
+        print("Hallo Welt")
+		
+        # # creating potential matrix with user input elements
+        # pot_mat = np.zeros((self.n, self.n))
+        
+        # # function to build potential matrix out of user adjusted array
+        # def mat_build(a):
+            # i = 0
+            # while i < self.n:
+                # pot_mat[i, i] = a[i]
+                # i += 1
+            # return pot_mat
+        
+        # for x in np.nditer(a, op_flags=['readwrite']):
+            # x[...] = self.factor * (x**2) + self.intersection
+        # mat_build(a)  # build pot_mat
         
         
-        # creating potential matrix with user input elements
-        pot_mat = np.zeros((self.n, self.n))
-        
-        # function to build potential matrix out of user adjusted array
-        def mat_build(a):
-            i = 0
-            while i < self.n:
-                pot_mat[i, i] = a[i]
-                i += 1
-            return pot_mat
-        
-        for x in np.nditer(a, op_flags=['readwrite']):
-            x[...] = factor * (x**2) + intersection
-        mat_build(a)  # build pot_mat
-        
-        
-        # creating body of the kinetic matrix with second derivate of the location
-        kin_mat = np.zeros((n, n))
+        # # creating body of the kinetic matrix with second derivate of the location
+        # kin_mat = np.zeros((n, n))
 
-        i = 0
-        while i < n:
-            kin_mat[i, i] = 2
-            i += 1
+        # i = 0
+        # while i < n:
+            # kin_mat[i, i] = 2
+            # i += 1
 
-        i = 0
-        while i < n-1:
-            kin_mat[i, i+1] = kin_mat[i+1, i] = -1
-            i += 1
-        print(kin_mat)
+        # i = 0
+        # while i < n-1:
+            # kin_mat[i, i+1] = kin_mat[i+1, i] = -1
+            # i += 1
+        # print(kin_mat)
 
-        # unit system and calculation of final hamiltonian matrix ham_mat
-        # factor 1000 for the unit system in order to reach meV
-        # unit_pot = ((1/2)*me*(3.5174*(10**29))*((10**-9))**2)/e  # potential matrix in meV
+        # # unit system and calculation of final hamiltonian matrix ham_mat
+        # # factor 1000 for the unit system in order to reach meV
+        # # unit_pot = ((1/2)*me*(3.5174*(10**29))*((10**-9))**2)/e  # potential matrix in meV
 
-        unit_kin = ((hbar**2)*1000)/(2*me*(10**-18)*e)
-        print(unit_kin, "\n", unit_pot)  # control print for unit
-        # dx for the derivate of the matrix
-        dx = l/n
-        # build the final hamilton matrix ham_mat
-        ham_mat = unit_kin * kin_mat * (1/(dx*dx)) + unit_pot * pot_mat
+        # unit_kin = ((hbar**2)*1000)/(2*me*(10**-18)*e)
+        # print(unit_kin, "\n", unit_pot)  # control print for unit
+        # # dx for the derivate of the matrix
+        # dx = l/n
+        # # build the final hamilton matrix ham_mat
+        # ham_mat = unit_kin * kin_mat * (1/(dx*dx)) + unit_pot * pot_mat
 
-        # calculate eigenvalues (stored in la) and eigenvectors (stored in v)
-        la, v = np.linalg.eigh(ham_mat)
-
-        
+        # # calculate eigenvalues (stored in la) and eigenvectors (stored in v)
+        # la, v = np.linalg.eigh(ham_mat)
 
         
-        return la, V
+
+        
+        # return la, V
         
         
     def calcualteGaussPotential(absdfjksdf):
