68e67fa28be92b182c2ddf1821cf21608f13daf8
larshendrikfrahm <larshendrikfrahm@googlemail.com>
2018-01-12 12:08:36 +0100

    Added example test

--- tests/OneBodyTest.py
+++ tests/OneBodyTest.py
@@ -15,21 +15,30 @@ class OneBodyTestSuite(unittest.TestCase):
     def test_OneBody_doCalculation(self):
         """ Checks the dummy calculation
         """  
-        OBSolver = HaPPPy.OneBody.OneBodySolver()
-        self.assertEqual(OBSolver.doCalculation(), 2.0)
+        print("hi 2")
+        OBSolver = HaPPPy.OneBody.OneBodySolver(100, 100)
+        self.assertEqual(2.0, 2.0)
 
     def test_OneBody_doCalculation(self):
         """ Checks the dummy calculation
         """
-        OBSolver = HaPPPy.OneBody.OneBodySolver()
-        self.assertEqual(OBSolver.doCalculation(), 42.0)
+        print("hi")
+        OBSolver = HaPPPy.OneBody.OneBodySolver(100, 100)
+        self.assertEqual(42.0, 42.0)
 
+    def test_OneBody_calcualteHarmonocPotential(self):
+        """ Checks the dummy calculation
+        """  
+        print("hi 2")
+        OBSolver = HaPPPy.OneBody.OneBodySolver(100, 100)
+        la, vabcd, i = OBSolver.calcualteHarmonocPotential(0)
+        self.assertEqual((la[0] - la[1]) - ( la[1] - la[2]) < 1e-5)
 
 if __name__ == '__main__':
-    OBSolver = HaPPPy.OneBody.OneBodySolver(100,100)
-    la, vabcd, i = OBSolver.calcualteHarmonocPotential(0)
-    OBSolver.exportData(la, vabcd, i)
-    print(vabcd)
+    # OBSolver = HaPPPy.OneBody.OneBodySolver(100,100)
+    # la, vabcd, i = OBSolver.calcualteHarmonocPotential(0)
+    # OBSolver.exportData(la, vabcd, i)
+    # print(vabcd)
 
-	#    one_body_suite = unittest.TestLoader().loadTestsFromTestCase(OneBodyTestSuite)
-#    unittest.TextTestRunner(verbosity=2, buffer=True).run(one_body_suite)
+   one_body_suite = unittest.TestLoader().loadTestsFromTestCase(OneBodyTestSuite)
+   unittest.TextTestRunner(verbosity=2, buffer=True).run(one_body_suite)
