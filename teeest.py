2178f612d4c10719ddd61b7cb0234940f1ac9128
Julius Roemer <jroemer@Studix20-lin.physnet.uni-hamburg.de>
2018-01-16 17:42:03 +0100

    Changed output Format to Gamma_L, Gamma_R(also in __init__.py). Included Transmissioncalculator. Still some work to do

---
+++ teeest.py
@@ -0,0 +1,4 @@
+import numpy
+a = [1, 5, 2]
+c=numpy.size(a)
+print(c)
--- tests/RatesTest.py
+++ tests/RatesTest.py
@@ -7,9 +7,14 @@ Energie_Einteilchen =  np.array([0.1, 3, 4]) #Energieeigenwerte von Gruppe 1
 Vinput = [0, 1, 3, 6] #Vorgegebene Tunnelbarriere
 Energie_Zweiteilchen = np.array([3, 4, 3.6]) #Energieegenwerte von Gruppe 2
 Koeeffizienten_Zweiteilchen= np.array([[[0, 1, 0],[1, 2, 0],[0, 0, 1]],[[1, 0 ,0],[0.2, 0.5, 0],[0.1, 0.3, 1]],[[0, 0, 0.5],[1, 0, 0.1],[0, 1, 0.1]]]) #Koeeffizientenmatrizen von Gruppe 2
-muL = 0
-muR= 0
+muL = 10
+muR= 2
 T= 10
+
+class DummyTransmission:
+
+    def calculate_transmission(self, a, b):
+        return 1
 
 class RatesTestSuite(unittest.TestCase):
     """A test class to the Rates module.
@@ -25,8 +30,9 @@ class RatesTestSuite(unittest.TestCase):
         """ Checks the dummy calculation
         """  
         Calc = HaPPPy.Rates.RateCalculator()
-        Calc.doCalculation(Energie_Einteilchen, Energie_Zweiteilchen, muL, muR, T, Vinput, Koeeffizienten_Zweiteilchen)
-        self.assertEqual(Calc.doCalculation(Energie_Einteilchen, Energie_Zweiteilchen, muL, muR, T, Vinput, Koeeffizienten_Zweiteilchen), 2.0)	
+        Dummy = DummyTransmission()
+        Calc.doCalculation(Energie_Einteilchen, Energie_Zweiteilchen, muL, muR, T, Vinput, Koeeffizienten_Zweiteilchen, Dummy)
+        self.assertEqual(Calc.doCalculation(Energie_Einteilchen, Energie_Zweiteilchen, muL, muR, T, Vinput, Koeeffizienten_Zweiteilchen, Dummy), 2.0)	
 	
 
 if __name__ == '__main__':
