4a3dac43f6733f0c5aa10dfc4ba453cabfccfb6f
fweissle <34276979+fweissle@users.noreply.github.com>
2018-01-18 23:21:33 +0100

    docu

--- HaPPPy/Transmission/Modules/GaussianWave.py
+++ HaPPPy/Transmission/Modules/GaussianWave.py
@@ -4,13 +4,16 @@ from scipy.constants import codata
 import matplotlib.pyplot as plt
 
 me   = codata.value("electron mass energy equivalent in MeV") * 1e8 ;  # Convert to milli eV/c^2
-hbar = codata.value("Planck constant over 2 pi in eV s")      * 1e19;  # Convert to 10*fs*millielectronvolts
+hbar = codata.value("Planck constant over 2 pi in eV s")      * 1e19;  # Convert to 10*femtoseconds*millielectronvolt
 
 
 
 class GaussianWave():
     
     def __init__(self, x_grid, symm, width, energy):
+        """
+        Creates the inital wavefunction in positionspace
+        """
 
         self.width = width
         self.symm = symm
@@ -27,11 +30,12 @@ class GaussianWave():
         return norm_x * np.exp(1j*self.k0*(x-self.symm)) * np.exp(- ( (x-self.symm)/self.width )**2 )
     
     def create_gauss_x_package(self):
+        """
+        Returns a gaussian wave package
+        """
         return np.array([self.create_gauss_at_x(pos) for pos in self.x_grid])
     
     def plot_x_package(self):
-        
-        #psi_abs_squared_x = np.multiply(self.x_package, self.x_package.conj()).real
         
         x_package = plt.plot(self.x_grid, np.abs(self.x_package))
         plt.title ("Propabilitydensity of gaussian package at time zero in position-space")
