3600953835ddb38da53960f813a8241736ae4258
Jakob Schmidt <jakschmi@Studix06.PHYSnet.uni-hamburg.de>
2018-01-25 15:21:49 +0100

    updated documentation

--- HaPPPy/Rates/RateCalculator.py
+++ HaPPPy/Rates/RateCalculator.py
@@ -13,27 +13,32 @@ class RateCalculator:
     
     """
     
-    This class calculates the rates of transitions between the possible one-body
-    and two-body states of an SET. It gives out the transition rates of events where
-    electrons tunnel through the left barrier onto the dot:
+    This class calculates the transition rates between the possible one-body
+    and two-body states of an SET.
+
+    It gives out the transition rates of events where
+    electrons tunnel through a barrier (here through the Left barrier) onto the dot:
+    
     :math:`\\Gamma^L_{\\alpha_N \\rightarrow \\beta_{N+1}} =
     \Gamma | \langle \\beta_{N+1} | \sum_{\\nu}`
     :math:`c_{\\nu}^{\\dagger} | \\alpha_N \\rangle |^2 n_F ( E_{\\beta_{N+1}} - E_{\\alpha_N} -\\mu_L )`
 
     where
-    :math:`\\beta_{N+1}` is the final one body or two body state and :math:`\\alpha_N` is the initial vacuum state or one-body state of the dot.
+    :math:`\\beta_{N+1}` is the final one-body or two-body state and :math:`\\alpha_N` is the initial vacuum state or one-body state of the dot.
     :math:`\\Gamma` includes the transmission coefficient of given tunneling event and the density of states function (DOS) on the dot
-    (see the description of the function :math:`\\Gamma`). :math:`n_F` is the fermifunction.
+    (see the description below). :math:`n_F` is the fermifunction.
+    
+    :math:`c_{\\nu}^{\\dagger}` is the creation operator. Applied to the :math:`| \\alpha_N \\rangle` - state, it creates an
+    artificial :math:`N + 1`-body state that is being projected onto the :math:`| \\beta_N \\rangle` - state.
 
-
-    Similiarly, the rates of events where an electron leaves the dot looks like this:
+    The rates of the transitions where the number of electrons on the dot decrease are similiarly calculated:
 
     :math:`\\Gamma^L_{\\alpha_N \\rightarrow \\beta_{N-1}} =
     \Gamma | \langle \\beta_{N-1} | \sum_{\\nu}`
-    :math:`c_{\\nu}^{\\dagger} | \\alpha_N \\rangle |^2 ( 1 - n_F ( E_{\\beta_{N-1}} - E_{\\alpha_N} -\\mu_L ))`.  
-    :math:`\\beta_{N-1}` and :math:`\\alpha_{N}` are, again, the final and initial states.
+    :math:`c_{\\nu} | \\alpha_N \\rangle |^2 ( 1 - n_F ( E_{\\beta_{N-1}} - E_{\\alpha_N} -\\mu_L ))`.  
+    :math:`\\beta_{N-1}` and :math:`\\alpha_{N}` are, again, the final and initial states. Here we use the annihilation operator :math:`c_{\\nu}`.
 
-    The Rates through the right barrier are calculated the same way (exchange :math:`\\mu_L` with :math:`\\mu_R`). 
+    The Rates through the right barrier are calculated the same way (one simply exchanges :math:`\\mu_L` with :math:`\\mu_R`). 
 
     """
     
@@ -47,26 +52,50 @@ class RateCalculator:
 
         """This function calculates the transition rates.
 
-        It needs the following inputs:
-        E1 (np.array): array of eigenvalues of the single-particle-states on the dot (calculated in the One Body Module)
-        E2 (np.array): array of eigenvalues of the two-particle-states on the dot (calculated in the Two Body Module)
-        muL (float): chemical potential of the source (main input)
-        muR(float): chemical potential of the drain (main input)
-        T (float): temperature (main input)#
-        pot(matrix): potential of the barriers.
-        V (np.array): tunnel-barrier-potential in form of an array for input into TransmissionCalculator (main input)
-        C (np.array): array of coefficients-matrices (calculated in the Two Body Module), where each matrix represents a two-particle-state in the single-particle-state-product-basis, that must be sorted according to the order of the E2 array (E2[i] must be the energy of the state represented by C[i])
-        TCalc (class): class in the Transmission Module that contains the function "calculate_transmission(E,V)" which calculates the transmission coefficient for a given energy difference E and tunnel-barrier-potential V
-        Density (function): function that gives us the density of states on source and drain (main input)
-
-        And gives the following output:
-        (Gamma_L, Gamma_R): Gamma_L and Gamma_R are two nxn-matrices (n=size(E1)+size(E2)+1) that contain the rates through the left barrier on the "source-side"(Gamma_L) and through the right barrier on the "drain-side"(Gamma_R), ready to be read out by the Master Equation Module.
-        Both matrices have the same format and only differ in their values, due to the different chemical potentials in source and drain.
-
-        Each output matrix has following format:
-        Gamma=[[0,Gamma_01,zeros],
-               [Gamma_10,zeros,Gamma_12]
-               [zeros, Gamma_21,zeros]] 
+        :param E1: Array of eigenvalues of the single-particle-states on the dot
+                   (calculated in the One Body Module).
+        :type E1: np.array
+        :param E2: Array of eigenvalues of the two-particle-states on the dot
+                   (calculated in the Two Body Module).
+        :type E2: np.array
+        :param muL: Chemical potential of the source (main input).
+        :type muL: float
+        :param muR: Chemical potential of the drain (main input).
+        :type muR: float
+        :param T: Temperature (main input).
+        :type T: float
+        :param pot: Potential of the barriers.
+        :type pot: matrix
+        :param V: tunnel-barrier-potential in form of an array for input into :code:`TransmissionCalculator`
+                  (main input)
+        :type V: np.array
+        :param C: array of coefficients-matrices (calculated in the Two Body Module), where each matrix
+                  represents a two-particle-state in the single-particle-state-product-basis,
+                  that must be sorted according to the order of the E2 array (E2[i] must be the energy
+                  of the state represented by C[i]).
+        :type C: np.array
+        :param TCalc: class in the Transmission Module that contains the function
+                      :code:`calculate_transmission(E,V)` which calculates the transmission coefficient
+                      for a given energy difference E and tunnel-barrier-potential V.
+        :type TCalc: class
+        :param Density: function that gives us the density of states on source and drain (main input).
+        :type Density: function
+        :param E0: Offset Value/ Vacuum energy.
+        :type E0: float
+        :param L: Length of tunnel barriers.
+        :type L: float
+        
+        :return: Returns (:math:`\\Gamma_L` , :math:`\\Gamma_R`):
+                 :math:`\\Gamma_L` and :math:`\\Gamma_R` are two nxn-matrices
+                 (:math:`n`=size(E1)+size(E2)+1) that contain the rates through the left barrier on
+                 the "source-side"(Gamma_L) and through the right barrier on the "drain-side"(Gamma_R).
+                 The both output matrices have following format:
+                 Gamma=[[0,Gamma_01,zeros],
+                       [Gamma_10,zeros,Gamma_12]
+                       [zeros, Gamma_21,zeros]] 
+        :rtype: list with two matrices
+        
+       
         """
         NEcut = len(E1) #we determine the number of single-particle states that we use
         VG=np.diag(pot)
@@ -97,9 +126,10 @@ class RateCalculator:
             return(f)
           
 
-	#This function is called by the Gamma_ij-equations and includes the transmission-coefficient for each tunnelling-event and the density of state function of the source and drain. 
+	#This function is called by the Gamma_ij-equations and includes the transmission-coefficient for each tunnelling-event
+        #and the density of state function of the source and drain. 
         def Gamma(Ea,Eb,V):
-             """:math::` \Gamma` includes the transmission coefficient and DOS: :math::` \Gamma = |t|^2*DOS`
+             """:math:`\\Gamma` includes the transmission coefficient and DOS: :math:`\Gamma = | t |^2 * \\delta`
 
              Ea(float): energy of initial state
              Eb(float): energy of final state
@@ -109,10 +139,9 @@ class RateCalculator:
              print(V)
              return (np.absolute(TCalc.calculate_transmission(Ea,V,dx)*Density.calculate_DensityofStates(np.absolute(Ea-Eb))))
                      
-        #These next four functions are used to calculate the rates of transmissions.
-        #Each function represents a specific kind of equation that deals with a certain kind of transmission;
-        #We distinguish between transitions, where the number of electrons on the dot changes from one to two(Gamma_12) and reverse(Gamma_21).
-        #And between transmissions where the number of electrons on the dot change from zero to one(Gamma_01) and reverse(Gamma_10).
+        #These next four functions are used to calculate the transition rates.Each function for a different ind of transition:
+        #We distinguish between transitions, in which the number of electrons on the dot changes from one to two(Gamma_12) and reverse(Gamma_21).
+        #And between transitions in which the number of electrons on the dot change from zero to one(Gamma_01) and reverse(Gamma_10).
 
         def Gamma_12(Ea,Eb,mu,T):
             """Calculates the rate of a transition from a one body state to a two body state
@@ -166,7 +195,8 @@ class RateCalculator:
             """
             return(Gamma(Ea,0,V)*(1-fermi(Ea-E0,mu,T)))
 
-        #creating the output matrices that later contain all the transition rates through either the left or the right barrier
+        #creating the output matrices that later contain all the transition rates through either
+        #the left or the right barrier
         Gamma_R=np.zeros((1+np.size(E1)+np.size(E2),1+np.size(E1)+np.size(E2)))
         Gamma_L=np.zeros((1+np.size(E1)+np.size(E2),1+np.size(E1)+np.size(E2)))
 
